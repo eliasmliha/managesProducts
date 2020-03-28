256450bdd9707fdd9cb6803fb48700ebf9db1c09
eliasmliha <eliasmlhaoui@gmail.com>
2020-03-28 14:19:46 +0100

    adding Product Repository interface

---
+++ src/main/kotlin/com/em/managesProducts/repository/ProductRepository.kt
@@ -0,0 +1,8 @@
+package com.em.managesProducts.repository
+
+import com.em.managesProducts.model.Product
+import org.springframework.data.jpa.repository.JpaRepository
+import org.springframework.stereotype.Repository
+
+@Repository
+interface ProductRepository :JpaRepository<Product,Long>
