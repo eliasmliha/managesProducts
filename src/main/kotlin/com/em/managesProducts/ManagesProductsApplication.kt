2639b01f8f3396a018766da3a94fd8df84bd2a5c
eliasmliha <eliasmlhaoui@gmail.com>
2020-03-28 15:40:04 +0100

    adding a functioning for generation a fake data on startup

--- src/main/kotlin/com/em/managesProducts/ManagesProductsApplication.kt
+++ src/main/kotlin/com/em/managesProducts/ManagesProductsApplication.kt
@@ -1,11 +1,42 @@
 package com.em.managesProducts
 
+import com.em.managesProducts.model.Product
+import com.em.managesProducts.repository.ProductRepository
+import org.springframework.beans.factory.annotation.Autowired
+import org.springframework.boot.CommandLineRunner
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
+import org.springframework.context.annotation.Bean
+import kotlin.random.Random
 
 @SpringBootApplication
-class ManagesProductsApplication
+class ManagesProductsApplication {
+
+    @Autowired
+    private val productRepository: ProductRepository? = null
+
+    @Bean
+    fun init() = CommandLineRunner {
+		productRepository?.let { it1 -> generateFakeData(it1) }
+    }
+
+}
 
 fun main(args: Array<String>) {
-	runApplication<ManagesProductsApplication>(*args)
+    runApplication<ManagesProductsApplication>(*args)
+}
+
+fun generateFakeData(productRepository: ProductRepository) {
+    for (i in 1..100) {
+        productRepository!!.save(
+                Product(
+                        i.toLong(),
+                        "title $i",
+                        "subtitle $i",
+                        Random.nextDouble(0.0, 100.0),
+                        "description $i",
+                        Random.nextInt(0, 10),
+                        "https://i.picsum.photos/id/$i/200/300.jpg"))
+        println("\n start $i")
+    }
 }
--- src/main/kotlin/com/em/managesProducts/model/Product.kt
+++ src/main/kotlin/com/em/managesProducts/model/Product.kt
@@ -4,21 +4,15 @@ import javax.persistence.Entity
 import javax.persistence.GeneratedValue
 import javax.persistence.GenerationType
 import javax.persistence.Id
-import javax.validation.constraints.NotBlank
+import javax.validation.constraints.NotEmpty
 
 @Entity
 data class Product(
         @Id @GeneratedValue(strategy = GenerationType.IDENTITY)
         val id: Long = 0,
-        @get: NotBlank
         val  title : String = "",
-        @get: NotBlank
         val  subtitle : String = "",
-        @get: NotBlank
-        val price : Double,
-        @get: NotBlank
+        val price : Double = 0.0,
         val description : String = "",
-        @get: NotBlank
-        val ratings : Int ,
-        @get: NotBlank
+        val ratings : Int  = 0,
         val images : String = "")
