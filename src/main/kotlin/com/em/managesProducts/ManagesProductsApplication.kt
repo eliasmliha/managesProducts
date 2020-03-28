7a6c08b0fbceebea645e9c96043f15d3ab61baa0
eliasmliha <eliasmlhaoui@gmail.com>
2020-03-28 11:53:02 +0100

    First commit

---
+++ src/main/kotlin/com/em/managesProducts/ManagesProductsApplication.kt
@@ -0,0 +1,11 @@
+package com.em.managesProducts
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class ManagesProductsApplication
+
+fun main(args: Array<String>) {
+	runApplication<ManagesProductsApplication>(*args)
+}
