1945b7900b987ddd1f088bed78dcc7ac638a6759
eliasmliha <eliasmlhaoui@gmail.com>
2020-04-02 20:10:42 +0200

    changing on rest API configuration for adding Id on response

--- src/main/kotlin/com/em/managesProducts/FakeDataGenerator.kt
+++ src/main/kotlin/com/em/managesProducts/FakeDataGenerator.kt
@@ -4,15 +4,21 @@ import com.em.managesProducts.model.Image
 import com.em.managesProducts.model.Product
 import com.em.managesProducts.repository.ImageRepository
 import com.em.managesProducts.repository.ProductRepository
+import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.boot.CommandLineRunner
+import org.springframework.data.rest.core.config.RepositoryRestConfiguration
 import org.springframework.stereotype.Component
 import kotlin.random.Random
 
+
 @Component
 class FakeDataGenerator(val productRepository: ProductRepository, val imageRepository: ImageRepository) : CommandLineRunner {
+    @Autowired
+    private val restConfiguration: RepositoryRestConfiguration? = null
 
 
     override fun run(vararg args: String?) {
+        restConfiguration!!.exposeIdsFor(Product::class.java, Image::class.java)
         for (i in 1..30) {
             val product = Product(0, "title ${i}", "subtitle ${i}", i.toDouble(), "description ${i}", i + 1)
             productRepository.save(product)
